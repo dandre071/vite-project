4defa4e2a9508072d81782c0977231f8ecbc987f
dandre071 <84488012+dandre071@users.noreply.github.com>
2024-08-24 16:56:22 -0500

    btns

--- src/components/Buttons/DeleteBtn.jsx
+++ src/components/Buttons/DeleteBtn.jsx
@@ -1,11 +1,18 @@
 import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 import ClearIcon from "@mui/icons-material/Clear";
 import { themeColors } from "../utils/configs";
+import { Trash2 } from "lucide-react";
 const DeleteBtn = ({ sx, onClick }) => {
   return (
     <>
       {/* <HighlightOffIcon sx={sx} onClick={onClick} />; */}
-      <ClearIcon
+      {/*  <ClearIcon
+        className="delete-btn"
+        sx={{ fontSize: 25, fontWeight: 800 }}
+        onClick={onClick}
+      /> */}
+
+      <Trash2
         className="delete-btn"
         sx={{ fontSize: 25, fontWeight: 800 }}
         onClick={onClick}
