6fb14e68e79859483dbdf5642dcfb3d43d51b31b
dandre071 <84488012+dandre071@users.noreply.github.com>
2024-09-16 12:53:54 -0500

    Update Root.jsx

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -5,42 +5,59 @@ import CartCounter from "../components/CartCounter";
 import Logo from "../components/Logo";
 import hexToRgba from "hex-to-rgba";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
-import { makeStyles } from "@mui/material";
-const style = makeStyles({
-  center: {
-    display: "flex",
-    justifyContent: "center",
-  },
-});
+import { Container, Stack } from "@mui/material";
+
+const style = {};
 const Root = () => {
   return (
     <ThemeProvider theme={customTheme}>
-      <div>
-        <Grid2 container>
-          <Grid2 item lg={12} sm={8} xs={12} md={10}>
-            <nav className="nav-bar">
-              <ul className="nav">
-                <Link to={"/"}>
-                  <Logo className="logo" width={50} />
-                </Link>
-                <CartCounter />
-                <li>
-                  <Link to={"/client-data"}>Cliente</Link>
-                </li>
+      <Stack>
+        <Grid2 container sx={{ width: "100vw" }} gridTemplateRows="10vh 1fr">
+          <Grid2
+            direction={"row"}
+            id="nav"
+            item
+            lg={12}
+            sm={8}
+            xs={12}
+            md={10}
+            sx={{ height: "100%", bgcolor: "red" }}
+          >
+            <ul style={{ display: "flex" }}>
+              <Link to={"/"}>
+                <Logo className="logo" width={50} />
+              </Link>
+              <CartCounter />
+              <li>
+                <Link to={"/client-data"}>Cliente</Link>
+              </li>
 
-                <li>
-                  <Link to={"/payment"}>Pay</Link>
-                </li>
-              </ul>
-            </nav>
+              <li>
+                <Link to={"/payment"}>Pay</Link>
+              </li>
+            </ul>
           </Grid2>
-          <Grid2 item lg={4} sm={8} xs={8} md={8}>
-            <main>
+          <Grid2
+            item
+            lg={4}
+            sm={8}
+            xs={8}
+            md={8}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "90vh",
+              width: "100vw",
+              bgcolor: "blue",
+            }}
+          >
+            <Container sx={{}}>
               <Outlet />
-            </main>
+            </Container>
           </Grid2>
         </Grid2>
-      </div>
+      </Stack>
     </ThemeProvider>
   );
 };
