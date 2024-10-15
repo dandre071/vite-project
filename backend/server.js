3900e8849eef023acbf5d47f8a366f29f3f08b59
dandre071 <84488012+dandre071@users.noreply.github.com>
2024-10-15 17:53:22 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const impresosDb = require("../backend/router");
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("hello");
 });
+app.use("api/v1/impresos", impresosDb);
 
 app.listen(port, () => console.log(`app listening in port ${port}`));
