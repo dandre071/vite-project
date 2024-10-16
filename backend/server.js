18e6c76520b1a3e899ec3697ed9760ed9946eee0
dandre071 <84488012+dandre071@users.noreply.github.com>
2024-10-16 18:20:41 -0500

    backend

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,10 @@ import { Router } from "express";
 const productos = Router;
 const app = express();
 const port = 3000;
-/* app.use(json); */
+app.use(json);
 app.get("/", (req, res) => {
   res.send("hello");
 });
-/* app.use("/api/v1/productos", productos); */
+app.use("/api/v1/productos", productos);
 
 app.listen(port, () => console.log(`app listening in port ${port}`));
