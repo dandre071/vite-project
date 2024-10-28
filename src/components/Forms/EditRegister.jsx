58a74cb82b29e69f127741788d2213dc4c18a53d
dandre071 <84488012+dandre071@users.noreply.github.com>
2024-10-27 21:48:49 -0500

    Update EditRegister.jsx

--- src/components/Forms/EditRegister.jsx
+++ src/components/Forms/EditRegister.jsx
@@ -1,20 +1,31 @@
+import { Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 
 const EditRegister = () => {
-  const id = location.pathname.match(/[0-9]/g);
+  const id = location.pathname.match(/[0-9]/g).join("");
   console.log(id);
-  const [reg, setReg] = useState([]);
+  const [reg, setReg] = useState(null);
   useEffect(() => {
     fetch("http://localhost:3000/api/v1/impresosDB/registro/" + id)
       .then((res) => res.json())
       .then((data) => {
-        /*  setReg(data); */
+        setReg(data);
         console.log(data);
       });
   }, []);
   console.log(reg);
   console.log();
-  return <div>EditRegister</div>;
+  return (
+    <div>
+      {reg && (
+        <div>
+          <p>{reg[0].id}</p>
+          <p>{reg && reg[0].nombre}</p>
+          <p>{reg && reg[0].nombre}</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default EditRegister;
