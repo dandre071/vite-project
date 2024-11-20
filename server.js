7c44339a10e367d6e019f4b960bcf477303757dc
dandre071 <84488012+dandre071@users.noreply.github.com>
2024-11-20 15:19:15 -0500

    server adjustments

---
+++ server.js
@@ -0,0 +1,34 @@
+/* import express from "express";
+import { router } from "../backend/router.js"; */
+import express, { json } from "express";
+
+import { router /* usersRouter  */ } from "./backend/router.js";
+import cors from "cors";
+import bodyParser from "body-parser";
+const impresosDB = router;
+/* const users = usersRouter; */
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:80",
+    "http://192.168.1.3",
+  ],
+};
+
+const app = express();
+const port = 3000;
+/* app.use(json); */
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+app.use("/api/v1/impresosDB", impresosDB);
+/* app.use("/api/v1/users", users); */
+app.listen(port, () => console.log(`app listening in port ${port}`));
